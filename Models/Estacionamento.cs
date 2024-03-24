df4031b511d808478d4cf62e37b01a0328fb9566
Lucas Augusto <lucas_lapada@hotmail.com>
2024-03-24 16:42:58 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoEstacionamentoDIO.Models
+{
+    class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        List<string> veiculo = new List<string>();//10 vagas
+
+        public Estacionamento(decimal pInicial, decimal pHora)
+        {
+            precoInicial = pInicial;
+            precoPorHora = pHora;
+
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculo.Add(Console.ReadLine());
+        }
+
+        public void RemoverVeiculo()
+        {
+            string placa;
+            decimal valor = 0;
+
+            Console.WriteLine("Digte a placa do veiculo que deseja remover:");
+            placa = Console.ReadLine();
+
+            if (veiculo.Contains(placa))
+            {
+                Console.Write("Quantas horas o de Estacionamento: ");
+                int hora = int.Parse(Console.ReadLine());
+                valor = precoInicial + (precoPorHora * hora);
+                veiculo.Remove(placa);
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valor.ToString("f2")}, por {hora} hora(s)");
+            
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculo.Count !=0)
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                foreach (var item in veiculo)
+                {
+                    Console.WriteLine($"Veiculo: {item}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+        public void Mostravagas()
+        {
+        }
+    }
+
+    //Pensar como fazer essa logica
+}
+
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using ProjetoEstacionamentoDIO.Models;
+using System;
+
+namespace ProjetoEstacionamentoDIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variaveis
+            decimal precoInicial = 0;
+            decimal precoHora = 0;
+            bool mostraMenu = true;
+            string opc = string.Empty;
+
+
+            //entrada de dados para futuros calculos
+            Console.Write("Seja bem vindo ao sistema de Estacionamento da DIO!\n" +
+                                    "Digete O valor da Inicial: ");
+            precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write($@"Agora digite o valor do preço por Hora: ");
+            precoHora = Convert.ToDecimal(Console.ReadLine());
+
+            //É preciso instanciar apenas depois de receber os valores iniciais, para que seja alocados na memória
+            Estacionamento esta = new Estacionamento(precoInicial, precoHora); 
+
+            //Isso faz com que o menu sempre apareça
+            while (mostraMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a sua opção:");
+                Console.WriteLine("1 - Cadastrar veículo");
+                Console.WriteLine("2 - Remover veículo");
+                Console.WriteLine("3 - Listar veículos");
+                Console.WriteLine("4 - Encerrar");
+                opc = Console.ReadLine();
+
+                switch (opc)
+                {
+                    case "1":
+                        esta.AdicionarVeiculo();
+                        break;
+
+                    case "2":
+                        esta.RemoverVeiculo();
+                        break;
+
+                    case "3":
+                        esta.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        esta.Mostravagas();
+                        break;
+                    case "5":
+                        mostraMenu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+
+                }
+
+                Console.WriteLine("Pressione uma tecla para continuar");
+                
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
